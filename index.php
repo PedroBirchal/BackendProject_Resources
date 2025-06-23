e8ed4d7371b5d6e34ddbebadb04e07ce392b19b2
Pedro de Lima Birchal <126815542+PedroBirchal@users.noreply.github.com>
2025-06-23 00:13:10 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include 'ListaDeItens.php';
+    
+    if(isset($_GET['suco'])){
+        $suco = $_GET['suco'];
+    }else{
+        $suco = 'SucoVerde';
+    }
+
+    switch($suco){
+        case 'SucoVerde':
+            $json = $listaDeItens[0];
+        break;
+        case 'SucoVermelho':
+            $json = $listaDeItens[1];
+        break;
+        case 'SucoAzul':
+            $json = $listaDeItens[2];
+        break;
+    }
+
+    echo $json;
+
+?>
